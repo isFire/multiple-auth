b34cb64e4743abf95476107324aef558b2c08e77
古尘 <guchen@dtstack.com>
2019-10-05 22:03:15 +0800

    初始化仓库

---
+++ src/main/java/com/dtstack/multiple/auth/MultipleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.dtstack.multiple.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleAuthApplication.class, args);
+    }
+
+}
