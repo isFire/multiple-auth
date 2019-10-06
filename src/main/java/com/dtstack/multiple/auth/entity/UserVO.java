9f3a093c41f35e773577e37095a47b58aa15c0a5
古尘 <guchen@dtstack.com>
2019-10-06 18:46:47 +0800

    update

---
+++ src/main/java/com/dtstack/multiple/auth/entity/UserVO.java
@@ -0,0 +1,9 @@
+package com.dtstack.multiple.auth.entity;
+
+/**
+ * @author <a href="mailto:guchen@dtstack.com">古尘 At 袋鼠云</a>.
+ * @description TODO
+ * @date 2019/10/6 18:34
+ */
+public class UserVO {
+}
